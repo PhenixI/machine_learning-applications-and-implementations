1c862bb9adbf401db6f874a1c266c1c7c2668472
PhenixI <zhanzhanlove.hi@gmail.com>
2014-06-25 19:53:55 +0800

    AdaBoost 算法的实现以及使用其对马殇病进行预测

--- AdaBoost/AdaBoost.py
+++ AdaBoost/AdaBoost.py
@@ -1,5 +1,7 @@
+# coding=gbk
 import numpy as np
 import scipy
+import math
 
 def loadSimpData():
     datMat = np.matrix( [[ 1. , 2.1],
@@ -10,21 +12,25 @@ def loadSimpData():
     classLabels = [1.0, 1.0, -1.0, -1.0, 1.0]
     return datMat,classLabels
 
+#Í¨¹ı·§Öµ±È½Ï¶ÔÊı¾İ½øĞĞ·ÖÀà
 def stumpClassify(dataMatrix,dimen,threshVal,threshIneq):
     retArray=np.ones((np.shape(dataMatrix)[0],1))
     if threshIneq == 'lt':
-        #å¾ˆå·§å¦™çš„åˆ†ç±»å†™æ³•
+        #ºÜÇÉÃîµÄ·ÖÀàĞ´·¨
         retArray[dataMatrix[:,dimen] <= threshVal] = -1.0
     else:
         retArray[dataMatrix[:,dimen] > threshVal]=-1.0
     return retArray
 
+#±éÀústumpClassify()º¯ÊıËùÓĞµÄ¿ÉÄÜÊäÈëÖµ£¬²¢ÕÒµ½Êı¾İ¼¯ÉÏ×î¼ÑµÄµ¥²ã¾ö²ßÊ÷¡£¼´ÈõÑ§Ï°Æ÷
 def buildStump(dataArr,classLabels,D):
     dataMatrix = np.mat(dataArr); labelMat = np.mat(classLabels).T
-    m,n = np.shape(dataMatrix)#mä¸ªä¾‹å­ï¼Œæ¯ä¸ªä¾‹å­nç»´
-    numSteps = 10.0; bestStump = {}; bestClasEst = np.mat(np.zeros((m,1)))
+    m,n = np.shape(dataMatrix)#m¸öÀı×Ó£¬Ã¿¸öÀı×ÓnÎ¬
+    numSteps = 10.0
+    bestStump = {}#ÓÃÓÚ´æ´¢¸ø¶¨È¨ÖØÏòÁ¿DÊ±ËùµÃµ½µÄ×î¼Ñµ¥²ã¾ö²ßÊ÷µÄÏà¹ØĞÅÏ¢¡£
+    bestClasEst = np.mat(np.zeros((m,1)))
     minError=np.inf
-    for i in range(n):
+    for i in range(n):#ÔÚÊı¾İ¼¯µÄËùÓĞÌØÕ÷ÉÏ±éÀú
         rangeMin = dataMatrix[:,i].min(); rangeMax = dataMatrix[:,i].max();
         stepSize = (rangeMax-rangeMin)/numSteps
         for j in range(-1,int(numSteps)+1):
@@ -34,6 +40,72 @@ def buildStump(dataArr,classLabels,D):
                     stumpClassify(dataMatrix,i,threshVal,inequal)
                 errArr = np.mat(np.ones((m,1)))
                 errArr[predictedVals == labelMat] = 0
+                #¼ÆËã¼ÓÈ¨´íÎóÂÊ
+                weightedError=D.T*errArr
+                #print "split:dim %d,thresh %.2f,thresh inequal: %s,the weighted error is %.3f" %(i,threshVal,inequal,weightedError)
+                if weightedError< minError:
+                    minError = weightedError
+                    bestClasEst=predictedVals.copy();
+                    bestStump['dim']=i
+                    bestStump['thresh']=threshVal
+                    bestStump['ineq']=inequal
+    return bestStump,minError,bestClasEst
+
+#ÊäÈë°üÀ¨Êı¾İ¼¯£¬Àà±ğ±êÇ©ÒÔ¼°µü´ú´ÎÊınumIt,Decision stump ,ÊÇAdaBoost ×îÁ÷ĞĞµÄµÄÈõ·ÖÀàÆ÷
+def adaBoostTrainDS(dataArr,classLabels,numIt=40):
+    weakClassArr=[]
+    m=np.shape(dataArr)[0]
+    D=np.mat(np.ones((m,1))/m)
+    aggClassEst=np.mat(np.zeros((m,1)))
+    for i in range(numIt):
+        bestStump,error,classEst=buildStump(dataArr,classLabels,D)
+        print "D:",D.T
+        alpha = float(0.5 * math.log((1.0-error)/max(error,1e-16)))#alphaÖµ¼ÆËã
+        bestStump['alpha']=alpha
+        weakClassArr.append(bestStump)
+        print "classEst: ",classEst.T
+        #¼ÆËãÏÂÒ»´Îµü´úÖĞµÄĞÂÈ¨ÖØD
+        expon= np.multiply(-1 * alpha * np.mat(classLabels).T,classEst)
+        D= np.multiply(D,np.exp(expon))
+        D= D/D.sum()
+        #Í¨¹ıaggClassEst ±äÁ¿±£³ÖÔËĞĞÊ±µÄÀà±ğ¹À¼ÆÖµÀ´ÊµÏÖ2..
+        aggClassEst += alpha * classEst
+        print "aggClassEst: ",aggClassEst.T
+        aggErrors= np.multiply(np.sign(aggClassEst) != np.mat(classLabels).T,np.ones((m,1)))
+        errorRate = aggErrors.sum()/m
+        print "total error: ",errorRate,"\n"
+        if errorRate == 0.0:break
+    return weakClassArr
+
+def adaClassify(datToClass,classifierArr):
+    dataMatrix = np.mat(datToClass)
+    m = np.shape(dataMatrix)[0]
+    aggClassEst = np.mat(np.zeros((m,1)))
+    for i in range(len(classifierArr)):
+        classEst = stumpClassify(dataMatrix,classifierArr[i]['dim'],classifierArr[i]['thresh'],classifierArr[i]['ineq'])
+        aggClassEst += classifierArr[i]['alpha']* classEst
+        print aggClassEst
+    return np.sign(aggClassEst)
+
+#Òı½øĞÂÊı¾İ
+def loadDataSet(fileName):
+    numFeat = len(open(fileName).readline().split('\t'))
+    dataMat = [];labelMat=[]
+    fr = open(fileName)
+    for line in fr.readlines():
+        lineArr = []
+        curLine = line.strip().split('\t')
+        for i in range(numFeat-1):
+            lineArr.append(float(curLine[i]))
+        dataMat.append(lineArr)
+        labelMat.append(float(curLine[-1]))
+    return dataMat,labelMat
+
+
+
+
+
+
 
 
 
